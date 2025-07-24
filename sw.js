2be17b9573b84ea8643189c473c808b222f264a7
Torakichin <syucyo@i.softbank.jp>
2025-07-24 17:58:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,20 +4,20 @@ var urlsToCache = [
     'Torakichin.github.io/pwa/app.js',
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(caches
-        .open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(caches
-        .match(event.request)
-        .then(function(response) {
-            return response ? response : fetch(event.request);
-        })
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
