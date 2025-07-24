d43b52d29f900c1751f16a1e7ebdcc6f095ee0eb
Torakichin <syucyo@i.softbank.jp>
2025-07-24 16:49:52 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+function omikuji() {
+  let msg1;
+  let msg2;
+  let random1;
+  let random2;
+
+  random1 = Math.floor(Math.random() * 2);
+  console.log(random1);
+
+  const result = ['大吉', '大凶'];
+
+  msg1 = result[random1];
+
+  random2 = Math.floor(Math.random() * 3);
+  console.log(random2);
+
+  const resultMsg = [
+      'これはサンプルどすえ',
+      'これはサンプル。',
+      'ポテチを食べましょう。',
+  ];
+
+  msg2 = resultMsg[random2];
+
+  document.getElementById('msgHeading').setAttribute('style', 'opacity: 1;');
+  setTimeout(function() {
+      document.getElementById('msg1').innerHTML = msg1;
+  }, 1000);
+  setTimeout(function() {
+      document.getElementById('msg2').innerHTML = msg2;
+  }, 2000);
+  document.getElementById('playBtn').setAttribute('disabled', 'disabled');
+  document.getElementById('resetBtn').removeAttribute('disabled');
+}
+
+function reset() {
+  document.getElementById('msgHeading').setAttribute('style', 'opacity: 0;');
+  document.getElementById('msg1').innerHTML = '';
+  document.getElementById('msg2').innerHTML = '';
+  document.getElementById('playBtn').removeAttribute('disabled');
+  document.getElementById('resetBtn').setAttribute('disabled', 'disabled');
+}
