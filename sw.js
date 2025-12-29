bda61fae70c7486656ab9db42b5a385347bc01df
Torakichin <syucyo@i.softbank.jp>
2025-12-29 19:00:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,33 @@
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    'Torakichin.github.io/pwa/',
-    'Torakichin.github.io/pwa/app.js',
+const CACHE_NAME = 'music-player-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './pict_stop.png',
+  './pict_play.png',
+  './bgm1.mp3',
+  './bgm2.mp3',
+  './bgm3.mp3',
+  './bgm4.mp3',
+  './bgm5.mp3',
+  './bgm6.mp3',
+  './bgm7.mp3'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+// オフライン時はキャッシュから応答
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
